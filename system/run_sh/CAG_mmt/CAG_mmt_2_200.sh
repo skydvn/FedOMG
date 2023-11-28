0aba554e5d8b89303c4a9a2a71709b7590ce02da
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2023-11-28 13:07:44 +0900

    update sh

---
+++ system/run_sh/CAG_mmt/CAG_mmt_2_200.sh
@@ -0,0 +1,4 @@
+python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 25 -mmt 0.9 -ss 30 -gam 0.5 -mstr resnet8
+python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 25 -mmt 0.7 -ss 30 -gam 0.5 -mstr resnet8
+python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 25 -mmt 0.3 -ss 30 -gam 0.5 -mstr resnet8
+python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 25 -mmt 0.1 -ss 30 -gam 0.5 -mstr resnet8
