76fc7c229ee630391a15fb8154754fd2cfd4a696
Daniel Nguyen <mduongbkhn@gmail.com>
2023-11-28 11:33:10 +0700

    fix

--- system/run_sh/CAG_lbs32/CAG_lbs32_1.sh
+++ system/run_sh/CAG_lbs32/CAG_lbs32_1.sh
@@ -1,5 +1,5 @@
+# local batch size
 
-"""local batch size"""
 python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32
 python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 15 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32
 python main.py -data Cifar10 -gr 200 -algo FedTest -nc 50 -ls 5 -car 100 -calr 50 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32
